93919db32e1c28452f763a1c1c247a7ac3ed910c
qigong <qigong@mogujie.com>
2017-11-20 19:14:05 +0800

    add xss

--- Plugin.php
+++ Plugin.php
@@ -79,6 +79,7 @@ class EditorMD_Plugin implements Typecho_Plugin_Interface
         $options = Helper::options();
         $cssUrl = $options->pluginUrl.'/EditorMD/css/editormd.css';
         $jsUrl = $options->pluginUrl.'/EditorMD/js/editormd.js';
+        $xssjsUrl = $options->pluginUrl.'/EditorMD/js/xss.js';
         $editormd = Typecho_Widget::widget('Widget_Options')->plugin('EditorMD');
         ?>
         <link rel="stylesheet" href="<?php echo $cssUrl; ?>" />
@@ -87,10 +88,11 @@ class EditorMD_Plugin implements Typecho_Plugin_Interface
             var uploadURL = '<?php Helper::security()->index('/action/upload?cid=CID'); ?>';
         </script>
         <script type="text/javascript" src="<?php echo $jsUrl; ?>"></script>
+        <script type="text/javascript" src="<?php echo $xssjsUrl; ?>"></script>
         <script>
             $(document).ready(function() {
-                alert($('#text'));
-                $('#text').html('<p>abc</p>');
+                alert($('#text').html());
+                $('#text').html(filterXSS($('#text').html()));
 
                 var textarea = $('#text').parent("p");
                 var isMarkdown = $('[name=markdown]').val()?1:0;
